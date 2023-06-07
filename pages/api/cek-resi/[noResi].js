ae7efe0577fb60bfbf05c80908e8db885279b55c
alfalinux <alfahmi.asman@gmail.com>
2023-06-08 03:18:44 +0700

    update cek resi

--- pages/api/cek-resi/[noResi].js
+++ pages/api/cek-resi/[noResi].js
@@ -1,4 +1,4 @@
-import { connectToMongoDB, findResi } from "../../../helpers/mongodbConnection";
+import { connectToMongoDB } from "../../../helpers/mongodbConnection";
 
 const handler = async (req, res) => {
   const { noResi } = req.query;
@@ -11,21 +11,73 @@ const handler = async (req, res) => {
     return;
   }
 
-  let result;
-  try {
-    result = await findResi(client, "data-resi", noResi.toUpperCase());
-  } catch (error) {
-    res.status(500).json({ status: res.statusCode, message: "Gagal terhubung ke client" });
+  if (req.method === "GET") {
+    const db = client.db("bista-app-v2");
+    let resi, manifest, suratJalan;
+
+    try {
+      resi = await db.collection("data-resi").findOne({ noResi: noResi });
+    } catch (error) {
+      resi = null;
+    }
+
+    try {
+      manifest = await db
+        .collection("data-manifest")
+        .findOne({ dataResi: { $elemMatch: { noResi: noResi } } });
+    } catch (error) {
+      manifest = null;
+    }
+
+    try {
+      suratJalan = await db
+        .collection("data-surat-jalan")
+        .find({ dataManifest: { $elemMatch: { noManifest: manifest.noManifest } } })
+        .toArray();
+    } catch (error) {
+      suratJalan = null;
+    }
+
+    const noResiCheck = !resi ? "" : resi.noResi;
+    const resiCreatedAt = !resi ? "" : resi.tglTransaksi;
+    const resiCreatedIn = !resi ? "" : resi.cabangAsal;
+    const noManifest = !manifest ? "" : manifest.noManifest;
+    const manifestCreatedAt = !manifest ? "" : manifest.manifestCreatedAt;
+    const manifestCreatedIn = !manifest ? "" : manifest.cabangAsal;
+    const manifestReceivedAt = !manifest ? "" : manifest.manifestReceivedAt;
+    const manifestReceivedIn = !manifest ? "" : manifest.coveranArea;
+    const listSuratJalan = !suratJalan
+      ? []
+      : suratJalan.map((d) => ({
+          noSuratJalan: d.noSuratJalan,
+          suratJalanCreatedAt: d.suratJalanCreatedAt,
+          suratJalanCreatedIn: d.cabangAsal,
+          suratJalanReceivedIn: d.cabangTujuan,
+          suratJalanReceivedAt: d.suratJalanReceivedAt,
+        }));
+    const listDelivery = !resi ? [] : resi.delivery;
+
+    res.status(201).json({
+      status: res.statusCode,
+      message: "Data ditemukan",
+      result: {
+        noResi: noResiCheck,
+        resiCreatedAt: resiCreatedAt,
+        resiCreatedIn: resiCreatedIn,
+        noManifest: noManifest,
+        manifestCreatedAt: manifestCreatedAt,
+        manifestCreatedIn: manifestCreatedIn,
+        manifestReceivedAt: manifestReceivedAt,
+        manifestReceivedIn: manifestReceivedIn,
+        listSuratJalan: listSuratJalan,
+        listDelivery: listDelivery,
+      },
+    });
     client.close();
-    return;
+  } else {
+    res.status(404).json({ status: res.statusCode, message: "Request method tidak sesuai" });
+    client.close();
   }
-
-  const response = result
-    ? { status: "201", message: "nomor resi ditemukan", result: result }
-    : { status: "404", message: "nomor resi tidak ditemukan", result: result };
-
-  res.status(201).json(response);
-  client.close();
 };
 
 export default handler;
--- pages/monitoring/cek-resi.js
+++ pages/monitoring/cek-resi.js
@@ -2,14 +2,98 @@ import Layout from "@/components/Layout";
 import DetailResi from "@/components/cek-resi/DetailResi";
 import SearchForm from "@/components/cek-resi/SearchForm";
 import TableTracker from "@/components/cek-resi/TableTracker";
+import { useEffect, useState } from "react";
+
+// const trackingResult = [
+//   { at: dataResi.resiCreatedAt, in: dataResi.resiCreatedIn, is: `create resi` },
+
+//   dataResi.manifestCreatedAt
+//     ? { at: dataResi.manifestCreatedAt, in: dataResi.manifestCreatedIn, is: `create manifest` }
+//     : null,
+
+//   dataResi.listSuratJalan.length > 0
+//     ? dataResi.listSuratJalan.map((d) => [
+//         { at: d.suratJalanCreatedAt, in: d.suratJalanCreatedIn, is: `create surat jalan` },
+//         d.suratJalanReceivedAt
+//           ? { at: d.suratJalanReceivedAt, in: d.suratJalanReceivedIn, is: `receive surat jalan` }
+//           : null,
+//       ])
+//     : null,
+
+//   dataResi.manifestReceivedAt
+//     ? { at: dataResi.manifestReceivedAt, in: dataResi.manifestReceivedIn, is: `receive manifest` }
+//     : null,
+
+//   dataResi.listDelivery.length > 0
+//     ? dataResi.listDelivery.map((d) => [
+//         {
+//           at: d.deliveryCreatedAt,
+//           by: d.dataKurir.nama + " - " + d.dataKurir.posisi + d.dataKurir.cabang + d.dataKurir.id,
+//           is: `create delivery`,
+//         },
+//         d.deliveryStatus.prosesAt
+//           ? {
+//               at: d.deliveryStatus.prosesAt,
+//               is: `${d.deliveryStatus.proses.toUpperCase()} - ${d.deliveryStatus.keterangan}`,
+//             }
+//           : null,
+//       ])
+//     : null,
+// ].flat(Infinity);
 
 const CekResiPage = (data) => {
-  console.log(data);
+  const [dataResi, setDataResi] = useState([]);
+
+  useEffect(() => {
+    setDataResi(
+      [
+        { at: data.result.resiCreatedAt, in: data.result.resiCreatedIn, is: `create resi` },
+
+        data.result.manifestCreatedAt
+          ? { at: data.result.manifestCreatedAt, in: data.result.manifestCreatedIn, is: `create manifest` }
+          : null,
+
+        data.result.listSuratJalan.length > 0
+          ? data.result.listSuratJalan.map((d) => [
+              { at: d.suratJalanCreatedAt, in: d.suratJalanCreatedIn, is: `create surat jalan` },
+              d.suratJalanReceivedAt
+                ? { at: d.suratJalanReceivedAt, in: d.suratJalanReceivedIn, is: `receive surat jalan` }
+                : null,
+            ])
+          : null,
+
+        data.result.manifestReceivedAt
+          ? { at: data.result.manifestReceivedAt, in: data.result.manifestReceivedIn, is: `receive manifest` }
+          : null,
+
+        data.result.listDelivery.length > 0
+          ? data.result.listDelivery.map((d) => [
+              {
+                at: d.deliveryCreatedAt,
+                by: d.dataKurir.nama + " - " + d.dataKurir.posisi + d.dataKurir.cabang + d.dataKurir.id,
+                is: `create delivery`,
+              },
+              d.deliveryStatus.prosesAt
+                ? {
+                    at: d.deliveryStatus.prosesAt,
+                    is: `${d.deliveryStatus.proses.toUpperCase()} - ${d.deliveryStatus.keterangan}`,
+                  }
+                : null,
+            ])
+          : null,
+      ]
+        .flat(Infinity)
+        .filter((val) => val !== null)
+        .sort((a, b) => new Date(a.at) - new Date(b.at))
+    );
+  }, [data]);
+
+  console.log(dataResi);
   return (
     <Layout>
       <div className="w-full p-4 flex flex-col gap-4">
         <SearchForm dataResi={data} />
-        {data.status == "201" ? <TableTracker dataResi={data} /> : null}
+        {data.status == "201" ? <TableTracker dataResi={dataResi} /> : null}
       </div>
     </Layout>
   );

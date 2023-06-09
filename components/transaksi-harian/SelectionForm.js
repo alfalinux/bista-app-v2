f46b2b30bf1ef8ab388afe1b1e840e9e2996c01d
alfalinux <alfahmi.asman@gmail.com>
2023-06-09 22:22:39 +0700

    add loading spinner

--- components/transaksi-harian/SelectionForm.js
+++ components/transaksi-harian/SelectionForm.js
@@ -3,8 +3,9 @@ import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import LoadingSpinner from "../utils/LoadingSpinner";
 
-const SelectionForm = () => {
+const SelectionForm = ({ isLoading, setIsLoading }) => {
   const { data } = useSession();
   const router = useRouter();
   const [cabang, setCabang] = useState("");
@@ -21,12 +22,15 @@ const SelectionForm = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    setIsLoading(true);
     router.push(`/monitoring/transaksi-harian?cabang=${cabang}&tglTransaksi=${tglTransaksi}`);
   };
 
-  //   console.log(cabang, tglTransaksi);
   return (
-    <form className="w-full p-4 bg-white dark:bg-gray-800 flex flex-col gap-4 border-[1px] border-gray-300 shadow-md rounded-lg">
+    <form
+      className="w-full p-4 bg-white dark:bg-gray-800 flex flex-col gap-4 border-[1px] border-gray-300 shadow-md rounded-lg"
+      onSubmit={submitHandler}
+    >
       <div className="w-full flex gap-2 items-center">
         <label className="w-52 text-sm text-gray-800 dark:text-gray-200" htmlFor="cabang">
           Cabang
@@ -67,14 +71,19 @@ const SelectionForm = () => {
         />
       </div>
       <div className="w-full">
-        <button
-          className="ml-auto bg-red-600 hover:bg-red-700 disabled:bg-gray-200 dark:disabled:bg-gray-700 font-semibold text-sm text-center text-white disabled:text-gray-400 dark:disabled:text-gray-800 p-2 rounded-md flex items-center justify-center gap-1 disabled:cursor-not-allowed"
-          onClick={submitHandler}
-          disabled={!cabang || !tglTransaksi}
-        >
-          <ArrowDownCircleIcon className="h-5" />
-          <p>Tampilkan Data</p>
-        </button>
+        {isLoading ? (
+          <div className="w-full flex justify-end items-center px-2 pt-2 pb-1">
+            <LoadingSpinner size="md" color="gray" />
+          </div>
+        ) : (
+          <button
+            className="ml-auto bg-red-600 hover:bg-red-700 disabled:bg-gray-200 dark:disabled:bg-gray-700 font-semibold text-sm text-center text-white disabled:text-gray-400 dark:disabled:text-gray-800 p-2 rounded-md flex items-center justify-center gap-1 disabled:cursor-not-allowed"
+            disabled={!cabang || !tglTransaksi}
+          >
+            <ArrowDownCircleIcon className="h-5" />
+            <p>Tampilkan Data</p>
+          </button>
+        )}
       </div>
     </form>
   );
--- components/transaksi-harian/TransaksiHarianContainer.js
+++ components/transaksi-harian/TransaksiHarianContainer.js
@@ -1,13 +1,39 @@
+import { useRouter } from "next/router";
 import Layout from "../Layout";
 import DataTable from "./DataTable";
 import SelectionForm from "./SelectionForm";
+import { useEffect, useState } from "react";
 
 const TransaksiHarianContainer = ({ dataTransaksi }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [dataTransaksi]);
+
   return (
     <Layout>
       <div className="w-full lg:w-[765px] xl:w-[960px] 2xl:w-[1216px] p-4 flex flex-col gap-4">
-        <SelectionForm />
-        <DataTable dataTransaksi={dataTransaksi} />
+        <SelectionForm isLoading={isLoading} setIsLoading={setIsLoading} />
+
+        {router.query.cabang && router.query.tglTransaksi && dataTransaksi.result.length > 0 ? (
+          <DataTable dataTransaksi={dataTransaksi} />
+        ) : null}
+
+        {router.query.cabang && router.query.tglTransaksi ? (
+          dataTransaksi.status == "201" ? (
+            dataTransaksi.result.length === 0 ? (
+              <p className="bg-red-300 dark:bg-red-800 text-sm text-center text-red-600 dark:text-red-300 p-2 shadow-md">
+                Tidak ada transaksi pada cabang dan tanggal tersebut...
+              </p>
+            ) : null
+          ) : (
+            <p className="bg-red-300 dark:bg-red-800 text-sm text-center text-red-600 dark:text-red-300 p-2 shadow-md">
+              {dataTransaksi.message}
+            </p>
+          )
+        ) : null}
       </div>
     </Layout>
   );
